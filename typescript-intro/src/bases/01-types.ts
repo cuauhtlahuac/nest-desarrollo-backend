def932976aa3a2e38ec879ff62b8bcd040b5798f
Rodrigo Aristeo <cuitlagua@gmail.com>
2026-08-15 00:13:23 -0600

    template string

--- typescript-intro/src/bases/01-types.ts
+++ typescript-intro/src/bases/01-types.ts
@@ -1,3 +1,18 @@
-export const nombre = "Ramiro";
+export let nombre = "Ramiro";
+export const age: number = 35;
+const isValid = true;
 
 console.log(nombre);
+
+export const templateString = ` Esto es un string
+multilínea
+que puede tener
+" comillas dobles
+' simples
+inyectar valores: ${ nombre }
+expresiones: ${ 1 + 1 }
+números: ${ age }
+booleanos: ${ isValid }
+`
+
+console.log(templateString);
